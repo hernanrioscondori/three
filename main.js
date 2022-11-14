b97151eaa1656368e5b8a7064bb583e5d0cc27b1
hernanrioscondori <103694996+hernanrioscondori@users.noreply.github.com>
2022-11-14 16:37:50 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,26 @@
-import {loadGLTF, loadVideo} from "../../libs/loader.js";
+import {loadGLTF} from "../../libs/loader.js";
+import {mockWithVideo} from '../../libs/camera-mock.js';
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
+    //mockWithVideo('../../assets/mock-videos/musicband1.mp4');
+
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: '../../assets/targets/sintel.mind',
+      imageTargetSrc: '../../assets/targets/musicband.mind',
     });
     const {renderer, scene, camera} = mindarThree;
 
-    const video = await loadVideo("../../assets/videos/sintel/sintel.mp4");
-    const texture = new THREE.VideoTexture(video);
+    const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
+    scene.add(light);
 
-    const geometry = new THREE.PlaneGeometry(1, 204/480);
-    const material = new THREE.MeshBasicMaterial({map: texture});
-    const plane = new THREE.Mesh(geometry, material);
+    const raccoon = await loadGLTF('../../assets/models/musicband-raccoon/scene.gltf');
+    raccoon.scene.scale.set(0.1, 0.1, 0.1);
+    raccoon.scene.position.set(0, -0.4, 0);
 
     const anchor = mindarThree.addAnchor(0);
-    anchor.group.add(plane);
-
-    anchor.onTargetFound = () => {
-      video.play();
-    }
-    anchor.onTargetLost = () => {
-      video.pause();
-    }
-    video.addEventListener( 'play', () => {
-      video.currentTime = 6;
-    });
+    anchor.group.add(raccoon.scene);
 
     await mindarThree.start();
     renderer.setAnimationLoop(() => {
@@ -36,3 +29,4 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   start();
 });
+
